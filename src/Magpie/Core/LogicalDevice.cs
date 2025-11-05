80120025ea557116e235c3409fd7a5f5681b5b00
mayli4 <sydsleeper@gmail.com>
2025-11-05 06:28:35 -0500

    wip: init logical device

--- src/Magpie/Core/LogicalDevice.cs
+++ src/Magpie/Core/LogicalDevice.cs
@@ -1,5 +1,19 @@
+using Magpie.Graphics;
+using Vortice.Vulkan;
+
 namespace Magpie.Core;
 
-public class LogicalDevice {
+public unsafe struct LogicalDevice : IDisposable {
+    public readonly PhysicalDevice PhysicalDevice;
+
+    internal readonly VkDevice Value;
+    public readonly nint Address => Value.Handle;
+
+    public LogicalDevice(PhysicalDevice physicalDevice) {
+        PhysicalDevice = physicalDevice;
+    }
     
+    public void Dispose() {
+        
+    }
 }
