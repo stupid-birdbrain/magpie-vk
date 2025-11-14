1a9368a534a3a7abc965995bf15153ccdf86078d
mayli4 <sydsleeper@gmail.com>
2025-11-14 03:02:43 -0500

    feat: color 2 vkclearvalue extension member

---
+++ src/Magpie/Utilities/_Extensions/ColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Standard;
+using Vortice.Vulkan;
+
+namespace Magpie.Utilities;
+
+public static class ColorExtensions {
+    extension(Color color) {
+        public VkClearValue ToVkClearValue() {
+            var vkClearColor = new VkClearColorValue(
+                color.R / 255f,
+                color.G / 255f,
+                color.B / 255f,
+                color.A / 255f
+            );
+
+            return new VkClearValue {
+                color = vkClearColor
+            };
+        }
+    }
+}
