7ac87e04ec453b41724a331fdd3db1011a74c03c
mayli4 <sydsleeper@gmail.com>
2025-11-14 03:03:37 -0500

    feat: queue submitting!

--- src/Magpie/Core/Queue.cs
+++ src/Magpie/Core/Queue.cs
@@ -18,8 +18,35 @@ public readonly unsafe struct Queue {
         Vulkan.vkGetDeviceQueue(LogicalDevice, FamilyIndex, Index, out Value);
     }
 
-    public readonly void Submit() {
-        
+    public readonly void Submit(
+        CommandBuffer commandBuffer,
+        Semaphore waitSemaphore,
+        Semaphore signalSemaphore,
+        Fence fence
+    )
+    {
+        VkSubmitInfo submitInfo = new() { sType = VkStructureType.SubmitInfo };
+
+        VkSemaphore waitSem = waitSemaphore.Value;
+        VkPipelineStageFlags waitStage =
+            VkPipelineStageFlags.ColorAttachmentOutput;
+        submitInfo.waitSemaphoreCount = 1;
+        submitInfo.pWaitSemaphores = &waitSem;
+        submitInfo.pWaitDstStageMask = &waitStage;
+
+        VkCommandBuffer cmd = commandBuffer.Value;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &cmd;
+
+        VkSemaphore signalSem = signalSemaphore.Value;
+        submitInfo.signalSemaphoreCount = 1;
+        submitInfo.pSignalSemaphores = &signalSem;
+
+        VkFence f = fence.Value;
+        var result = Vulkan.vkQueueSubmit(Value, 1, &submitInfo, f);
+        if (result != VkResult.Success) {
+            throw new Exception($"failed to submit to queue!: {result}");
+        }
     }
     
     public readonly VkResult TryPresent(Semaphore semaphore, Swapchain swapchain, uint imageIndex) 
