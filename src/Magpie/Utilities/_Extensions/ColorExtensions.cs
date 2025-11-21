9b4c6cb6ff65c2105fce02d6dde514dd88a1585c
mayli4 <sydsleeper@gmail.com>
2025-11-21 02:46:33 -0500

    feat: color extension member

--- src/Magpie/Utilities/_Extensions/ColorExtensions.cs
+++ src/Magpie/Utilities/_Extensions/ColorExtensions.cs
@@ -4,31 +4,18 @@ using Vortice.Vulkan;
 namespace Magpie.Utilities;
 
 public static class ColorExtensions {
-    // extension(Color color) {
-    //     public VkClearValue ToVkClearValue() {
-    //         var vkClearColor = new VkClearColorValue(
-    //             color.R / 255f,
-    //             color.G / 255f,
-    //             color.B / 255f,
-    //             color.A / 255f
-    //         );
-    //
-    //         return new VkClearValue {
-    //             color = vkClearColor
-    //         };
-    //     }
-    // }
+    extension(Color color) {
+        public VkClearValue ToVkClearValue() {
+            var vkClearColor = new VkClearColorValue(
+                color.R / 255f,
+                color.G / 255f,
+                color.B / 255f,
+                color.A / 255f
+            );
     
-    public static VkClearValue ToVkClearValue(this Color color) {
-        var vkClearColor = new VkClearColorValue(
-            color.R / 255f,
-            color.G / 255f,
-            color.B / 255f,
-            color.A / 255f
-        );
-
-        return new VkClearValue {
-            color = vkClearColor
-        };
+            return new VkClearValue {
+                color = vkClearColor
+            };
+        }
     }
 }
