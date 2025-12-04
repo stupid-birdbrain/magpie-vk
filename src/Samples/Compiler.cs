2c5dbb2b1287fc6c3a7725ff7d026105f379481d
mayli4 <sydsleeper@gmail.com>
2025-12-04 10:40:56 -0500

    feat: new (still garbage) compiler

--- src/Samples/Compiler.cs
+++ src/Samples/Compiler.cs
@@ -1,4 +1,5 @@
 using ShaderCompilation;
+using ShaderCompilation.Models;
 using Vortice.SpirvCross;
 
 namespace Samples;
@@ -9,44 +10,44 @@ public unsafe class ShaderCompiler : IDisposable {
     public CompilerCtx CompilerCtx;
 
     public ShaderCompiler() {
-        CompilerCtx = new CompilerCtx();
-        CompilerCtx._options.CustomIncludeHandler = DefaultIncludeHandler;
+        CompilerCtx = new CompilerCtx(Backend.GLSL);
+        CompilerCtx.Options.CustomIncludeHandler = DefaultIncludeHandler;
     }
 
-    public void SetCustomIncludeHandler(CompilerCtx.Options.IncludeHandler handler) {
-        CompilerCtx._options.CustomIncludeHandler = handler;
+    public void SetCustomIncludeHandler(CompilerOptions.IncludeHandler handler) {
+        CompilerCtx.Options.CustomIncludeHandler = handler;
     }
 
-    private CompilerCtx.IncludeResult DefaultIncludeHandler(string requestedSource, string requestingSource, IncludeType type) {
+    private Include DefaultIncludeHandler(string requestedSource, string requestingSource, IncludeType type) {
         string resolvedPath;
         string requestingSourceDir = Path.GetDirectoryName(requestingSource) ?? string.Empty;
         if (type == IncludeType.Relative && !string.IsNullOrEmpty(requestingSourceDir)) {
             resolvedPath = Path.Combine(requestingSourceDir, requestedSource);
             if (File.Exists(resolvedPath)) {
-                return new CompilerCtx.IncludeResult(resolvedPath, File.ReadAllText(resolvedPath));
+                return new Include(resolvedPath, File.ReadAllText(resolvedPath));
             }
         }
 
         resolvedPath = "resources";
         if (File.Exists(resolvedPath)) {
-            return new CompilerCtx.IncludeResult(resolvedPath, File.ReadAllText(resolvedPath));
+            return new Include(resolvedPath, File.ReadAllText(resolvedPath));
         }
 
         if (File.Exists(requestedSource)) {
-            return new CompilerCtx.IncludeResult(requestedSource, File.ReadAllText(requestedSource));
+            return new Include(requestedSource, File.ReadAllText(requestedSource));
         }
         
         string errorMessage = $"could not resolve include '{requestedSource}' requested by '{requestingSource}'.";
         Console.Error.WriteLine(errorMessage);
-        return new CompilerCtx.IncludeResult(requestedSource, errorMessage, isError: true);
+        return new Include(requestedSource, "", errorMessage);
     }
 
     public ReadOnlySpan<byte> CompileShader(string path, ShaderKind kind, bool debug = false) {
-        CompilerCtx._options.SetSourceLanguage(LangKind.Glsl);
-        CompilerCtx._options.SetTargetEnv(TargetEnv.Vulkan, Vortice.Vulkan.VkVersion.Version_1_3);
-        CompilerCtx._options.SetTargetSpirv(SpirvVersion.Spirv13);
-        CompilerCtx._options.SetGenerateDebugInfo(debug);
-        CompilerCtx._options.SetOptimizationLevel(debug ? OptimizationLevel.None : OptimizationLevel.Performance);
+        CompilerCtx.Options.SetSourceLanguage(LangKind.Glsl);
+        CompilerCtx.Options.SetTargetEnv(TargetEnv.Vulkan, Vortice.Vulkan.VkVersion.Version_1_3);
+        CompilerCtx.Options.SetTargetSpirv(SpirvVersion.Spirv13);
+        CompilerCtx.Options.SetGenerateDebugInfo(debug);
+        CompilerCtx.Options.SetOptimizationLevel(debug ? OptimizationLevel.None : OptimizationLevel.Performance);
 
         string shaderCode = File.ReadAllText(path);
 
@@ -61,14 +62,13 @@ public unsafe class ShaderCompiler : IDisposable {
             if (compileResult.NumWarnings > 0) {
                  Console.WriteLine($"shader compilation warnings for {path}:\n{compileResult.ErrorMessage}");
             }
-            
 
             return compileResult.GetBytesCopy();
         }
     }
     
-    public ReflectedShaderData ReflectShader(byte[] spirvBytes, Backend backend) {
-        return CompilerCtx.AttemptSpvReflect(spirvBytes, backend);
+    public ReflectedShaderData ReflectShader(byte[] spirvBytes) {
+        return CompilerCtx.AttemptSpvReflect(spirvBytes);
     }
 
     public void Dispose() {
